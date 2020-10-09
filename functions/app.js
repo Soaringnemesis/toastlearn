4c01933a9ef56e35416dd303be51483e484e9233
Drew Gifford <legos30000@hotmail.com>
2020-10-09 19:01:27 -0400

    Make front page

--- functions/app.js
+++ functions/app.js
@@ -1,10 +1,32 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const engines = require("consolidate");
+const firebase = require("firebase-admin");
+const path = require("path");
 const PORT = 80;
 
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
 const app = express();
+
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
+
+let staticOptions = {
+    dotfiles: "ignore",
+    etag: true,
+    index: false,
+    redirect: false
+}
+
+app.use(express.static("public", staticOptions));
+
+
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.render("index", {});
 });
 
 exports.app = functions.https.onRequest(app);
