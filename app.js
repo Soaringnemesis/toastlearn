e871274e43276d0967379125b3474e8cc42e72f9
Drew Gifford <legos30000@hotmail.com>
2020-10-09 18:41:20 -0400

    Make app a firebase function

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+const functions = require("firebase-functions");
+const express = require("express");
+const PORT = 80;
+
+const app = express();
+app.get("/", (request, response) => {
+
+});
+
+exports.app = functions.https.onRequest(app);
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT", PORT);
 });
