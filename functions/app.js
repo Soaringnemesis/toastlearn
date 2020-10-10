6907ba653c280bdd5a0668cb5317213a4e084710
Drew Gifford <legos30000@hotmail.com>
2020-10-10 05:07:34 -0400

    All of the toasts!

--- functions/app.js
+++ functions/app.js
@@ -12,6 +12,8 @@ const bodyParser    = require("body-parser");
 
 
 admin.initializeApp();
+const db = admin.firestore();
+
 
 const app = express();
 
@@ -23,6 +25,19 @@ let staticOptions = {
     redirect: false
 }
 
+//I don't care if this gets leaked. Who will use it anyways?
+/*const ALGOLIA_APP_ID = "OBHENVV9DR";
+const ALGOLIA_ADMIN_KEY = "3a7b89776da3b87012ef76126a9783a6";
+const ALGOLIA_INDEX_NAME = "subjects";
+
+exports.addFirestoreDataToAlgolia = functions.https.onRequest((req,res) => {
+	admin.firestore().collection("subjects").get().then((docs)=> {
+		docs.forEach((doc) => {
+			let name = doc.data();
+			name.objectID = doc.id;
+		})
+	})
+})*/
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -45,15 +60,38 @@ app.get("/", (req, res) => {
 	  .then((decodedClaims) => {
 		var uid = decodedClaims.user_id;
 		admin.auth().getUser(uid).then(function(userRecord) {
+			
 			// See the UserRecord reference doc for the contents of userRecord.
-			console.log(userRecord);
 			data = userRecord.toJSON();
-			res.render("index", {title: "ToastLearn", user: data});
+
+			db.collection("subjects").get()
+			.then(query=>{
+				let json = query.docs.map(doc=>{
+					let x = doc.data()
+						x['_id']=doc.id;
+						return x;
+				});
+				console.log("LOOKIE HERE!");
+				console.log(json);
+				res.render("index", {title: "All Toasts | ToastLearn", user: data, subjects: json});
+				
+			})
 		});
 	  })
 	  .catch(error => {
 		// Session cookie is unavailable or invalid. Force user to login.
-		res.render("index", {title: "ToastLearn", user: false});
+		db.collection("subjects").get()
+			.then(query=>{
+				let json = query.docs.map(doc=>{
+					let x = doc.data()
+						x['_id']=doc.id;
+						return x;
+				});
+				console.log("LOOKIE HERE!");
+				console.log(json);
+				res.render("index", {title: "All Toasts | ToastLearn", user: false, subjects: json});
+				
+			});
 	  });
 });
 //Login page
@@ -65,14 +103,13 @@ app.get("/login", (req, res) => {
 		var uid = decodedClaims.user_id;
 		admin.auth().getUser(uid).then(function(userRecord) {
 			// See the UserRecord reference doc for the contents of userRecord.
-			console.log(userRecord);
 			data = userRecord.toJSON();
-			res.render("login", {title: "ToastLearn", user: data});
+			res.render("login", {title: "Log In | ToastLearn", user: data});
 		});
 	  })
 	  .catch(error => {
 		// Session cookie is unavailable or invalid. Force user to login.
-		res.render("login", {title: "ToastLearn", user: false});
+		res.render("login", {title: "Log In | ToastLearn", user: false});
 	  });
 });
 //Register page
@@ -84,14 +121,13 @@ app.get("/register", (req, res) => {
 		var uid = decodedClaims.user_id;
 		admin.auth().getUser(uid).then(function(userRecord) {
 			// See the UserRecord reference doc for the contents of userRecord.
-			console.log(userRecord);
 			data = userRecord.toJSON();
-			res.render("register", {title: "ToastLearn", user: data});
+			res.render("register", {title: "Register | ToastLearn", user: data});
 		});
 	  })
 	  .catch(error => {
 		// Session cookie is unavailable or invalid. Force user to login.
-		res.render("register", {title: "ToastLearn", user: false});
+		res.render("register", {title: "Register | ToastLearn", user: false});
 	  });
 });
 
@@ -103,7 +139,6 @@ app.get("/toast", (req,res) => {
 		var uid = decodedClaims.user_id;
 		admin.auth().getUser(uid).then(function(userRecord) {
 			// See the UserRecord reference doc for the contents of userRecord.
-			console.log(userRecord);
 			data = userRecord.toJSON();
 			res.render("toast", {title: "ToastLearn", user: data});
 		});
@@ -112,7 +147,57 @@ app.get("/toast", (req,res) => {
 		// Session cookie is unavailable or invalid. Force user to login.
 		res.redirect('/login');
 	  });
-})
+});
+
+app.get('/toast/:id', function(req, res) {
+	var id = req.params.id;
+	const sessionCookie = req.cookies.session || '';
+	admin.auth().verifySessionCookie(
+	  sessionCookie, true /** checkRevoked */)
+	  .then((decodedClaims) => {
+		var uid = decodedClaims.user_id;
+		admin.auth().getUser(uid).then(function(userRecord) {
+			// See the UserRecord reference doc for the contents of userRecord.
+			db.collection('subjects').doc(id).get().then(function(sub){
+				console.log(sub.data());
+				data = userRecord.toJSON();
+
+
+				db.collection("subjects/"+id+"/resources").get()
+				.then(query=>{
+					let json = query.docs.map(doc=>{
+						let x = doc.data()
+							x['_id']=doc.id;
+							return x;
+					});
+					
+					res.render("toast", {title: sub.data().name+" | ToastLearn", user: data, toast: sub.data(), resources: json});
+				});
+			});
+	  	})
+	  .catch(error => {
+		// Session cookie is unavailable or invalid. Force user to login.
+
+		//TEMP
+		db.collection('subjects').doc(id).get().then(function(sub){
+			console.log(sub.data());
+			data = userRecord.toJSON();
+
+
+			db.collection("subjects/"+id+"/resources").get()
+			.then(query=>{
+				let json = query.docs.map(doc=>{
+					let x = doc.data()
+						x['_id']=doc.id;
+						return x;
+				});
+				
+				res.render("toast", {title: sub.data().name+" | ToastLearn", user: false, toast: sub.data(), resources: json});
+			});
+		});
+	  });
+});
+});
 
 app.get('/profile', (req, res) => {
 	
@@ -122,10 +207,7 @@ app.get('/profile', (req, res) => {
 	  .then((decodedClaims) => {
 		var uid = decodedClaims.user_id;
 		admin.auth().getUser(uid).then(function(userRecord) {
-			// See the UserRecord reference doc for the contents of userRecord.
-			console.log(userRecord);
-			data = userRecord.toJSON();
-			res.render("index", {title: "ToastLearn", user: data});
+			res.redirect("/");
 		});
 	  })
 	  .catch(error => {
@@ -142,6 +224,11 @@ app.get("/login", function (req, res) {
 	res.clearCookie("session");
 	res.redirect("/login");
   });
+
+
+app.get('/searchQuery', function(req, res){
+	var val = req.query.search;
+});
 
 app.post("/sessionLogin", (req, res) => {
 	const idToken = req.body.idToken.toString();
@@ -162,6 +249,9 @@ app.post("/sessionLogin", (req, res) => {
 		}
 	  );
   });
+
+  function getSubjectsAsJSON(){
+  }
 
 
 
