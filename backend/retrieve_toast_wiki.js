153002959c382cd87cc5a01e3977f33e55728f19
hlopezes14 <humbertothree@gmail.com>
2020-10-11 01:45:31 -0700

    Wiki will a add a larger snippet

--- backend/retrieve_toast_wiki.js
+++ backend/retrieve_toast_wiki.js
@@ -2,6 +2,7 @@ const wiki = require('wikijs').default;
 var request = require('request');
 
 function retrieve_wiki_data(topic){
+    //URL to search for list of pages
     var url = `https://en.wikipedia.org/w/api.php?action=query&prop=extract&list=search&srsearch=${topic}&format=json`;
     //console.log(url);
     request(url, function (err, response, body) {
@@ -13,23 +14,50 @@ function retrieve_wiki_data(topic){
        } else {
             const data = JSON.parse(body)
 
+            // get the first page as it is the most relevant
             var page = data.query.search[0]
 
             var page_url = `https://en.wikipedia.org/wiki/${page.title}`;
-            var article = {
-                "title" : page.title,
-                "snippet" : page.snippet,
-                "url " : page_url
-            }
 
             //console.log('PAGE:', article)
+            console.log(page.title)
 
-            return page.title
+            // this is the url to request information about the page to be used
+            var curr_page_url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${page.title}&exchars=500&explaintext=1`;
+            request(curr_page_url, function(err, response,body2) {
+                if(err){
+                    var error = "cannot connect to the server";
+                    console.log(error);
+        
+               } else {
+                    const data = JSON.parse(body2)
+
+                    // theres only one item in this for loop so snippet will have the description
+                    var snippet = ''
+                    for (id in data.query.pages){
+                        snippet = data.query.pages[id].extract
+                    }
+
+                    //console.log(snippet)
+                    //console.log(page.title)
+                    //console.log(page_url)
+
+                    // Uncomment below and Add to database here
+                    // var article = {
+                    //"title" : page.title,
+                    //"snippet" : snippet,
+                    //"url " : page_url
+                    //"type" : "wiki"
+                    //}
+
+               }
+            })
+
         }
-       });
+       })
 
 
 }
-var query = "Los Angeles Basketball"
+var query = "Cleveland football"
 var ret = retrieve_wiki_data(query)
 console.log("RET:",ret)
